942390afe779f02d38c0977c6e66c65e4f3b5119
gianxr85 <giancarlo.pareja@gmail.com>
2026-09-07 13:42:07 -0300

    Agregar módulo de emisión ARCA producción general

---
+++ functions/arcaGeneral.js
@@ -0,0 +1,5 @@
+"use strict";
+// Emisión productiva general ARCA. La implementación se conecta desde index.js.
+// Punto de venta fiscal fijo: 00009.
+const PUNTO_VENTA_ARCA_PRODUCCION = 9;
+module.exports = { PUNTO_VENTA_ARCA_PRODUCCION };
