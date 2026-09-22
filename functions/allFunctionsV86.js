4291f5f97491165625cb7a47784547b35c4a13bc
gianxr85 <giancarlo.pareja@gmail.com>
2026-09-22 16:20:29 -0300

    Export backend Base Madre synchronizer

--- functions/allFunctionsV86.js
+++ functions/allFunctionsV86.js
@@ -1,3 +1,4 @@
 "use strict";
 Object.assign(exports, require("./index.js"));
 Object.assign(exports, require("./facturacionRecuperarV86.js"));
+Object.assign(exports, require("./baseMadreBackendV120.js"));
