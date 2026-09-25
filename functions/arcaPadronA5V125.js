346ed4e54a19417c464589bacd73a37f7cbe7369
gianxr85 <giancarlo.pareja@gmail.com>
2026-09-25 14:50:31 -0300

    chore(firebase): disparar deploy de constancia de inscripcion ARCA

--- functions/arcaPadronA5V125.js
+++ functions/arcaPadronA5V125.js
@@ -15,6 +15,7 @@ const allowed = defineSecret("ARCA_ALLOWED_EMAILS");
 const WSAA = "https://wsaa.afip.gov.ar/ws/services/LoginCms";
 const PADRON = "https://aws.afip.gov.ar/sr-padron/webservices/personaServiceA5";
 const SERVICE = "ws_sr_constancia_inscripcion";
+// ARCA 2026: reemplaza al servicio historico ws_sr_padron_a5.
 const ORIGINS = new Set([
   "https://tiz-publicidad.github.io",
   "https://tiz---app.web.app",
